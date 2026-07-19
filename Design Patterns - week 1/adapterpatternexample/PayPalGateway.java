46005b7a802d6d18f1d4b0e7ce65d19009ddd345
Bhagyasri Andhavarapu <bhagyasriandhavarapu@gmail.com>
2026-07-19 21:25:41 +0530

    Initial commit

--- Design Patterns - week 1/adapterpatternexample/PayPalGateway.java
+++
@@ -1,7 +0,0 @@
-public class PayPalGateway {
-
-    public void pay(double amount) {
-
-        System.out.println("Paid ₹" + amount + " using PayPal.");
-    }
-}
--- Design Patterns - week 1/dependencyinjectionexample/CustomerRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-public class CustomerRepositoryImpl
-        implements CustomerRepository {
-
-    @Override
-    public String findCustomerById(int id) {
-
-        return "Customer Found : Bhagyasri (ID: " + id + ")";
-    }
-}
